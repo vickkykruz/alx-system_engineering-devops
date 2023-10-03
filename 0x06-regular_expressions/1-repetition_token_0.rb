1d60c8c30e56bf68a216dea50dac3e052ee223ea
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-10-03 07:15:07 -0700

    Correction Made task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # This is ruby script that find the regular expression that will match the
 # above cases
-puts ARGV[0].scan(\hbt{2,5}n\).join;
+puts ARGV[0].scan(/hbt{2,5}n/).join
