bdafc3106da7d043fd82f3bd211eb2a8bddfa1c5
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-24 09:04:39 -0800

    Checking

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
   content => 'I love Puppet'
 }
