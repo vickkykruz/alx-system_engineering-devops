53f0dd47832bea94278f063f677ad763ed6d368c
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-01-02 11:53:29 -0800

    Modification of file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0, extend your Python script to export data in the CSV format.
+Using what you did in the task #0, extend your Python script to export data in
+the CSV format.
 
 Requirements:
 
     => Records all tasks that are owned by this employee
-    => Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    => Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS",
+     "TASK_TITLE"
     => File name must be: USER_ID.csv
 """
 
