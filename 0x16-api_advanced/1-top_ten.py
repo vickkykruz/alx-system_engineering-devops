dd933721ccd5b1a8454d0b79c227d7bdbc1059b7
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-01-16 06:11:14 -0800

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ This is function that return the top ten posts """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'CustomUserAgent/1.0'}
+
+    requestData = requests.get(url, headers=headers)
+
+    if requestData.status_code == 200:
+
+        data = requestData.json()
+        for title in data['data']['children']:
+            print(title['data']['title'])
+    else:
+        printf("None")
